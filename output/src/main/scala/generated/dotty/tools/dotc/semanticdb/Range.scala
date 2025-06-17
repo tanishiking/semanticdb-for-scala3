7bc92d0c8a34e1b0e2a89ad0db2e09dc803d024f
Scala Steward <scala_steward@virtuslab.com>
2025-06-17 18:51:44 +0000

    Reformat with scalafmt 3.7.17
    Executed command: scalafmt --non-interactive

--- output/src/main/scala/generated/dotty/tools/dotc/semanticdb/Range.scala
+++ output/src/main/scala/generated/dotty/tools/dotc/semanticdb/Range.scala
@@ -14,90 +14,96 @@ final case class Range(
     startCharacter: _root_.scala.Int = 0,
     endLine: _root_.scala.Int = 0,
     endCharacter: _root_.scala.Int = 0
-    )  extends SemanticdbGeneratedMessage  derives CanEqual {
-    @transient @sharable
-    private var __serializedSizeMemoized: _root_.scala.Int = 0
-    private def __computeSerializedSize(): _root_.scala.Int = {
-      var __size = 0
-      
-      {
-        val __value = startLine
-        if (__value != 0) {
-          __size += SemanticdbOutputStream.computeInt32Size(1, __value)
-        }
-      };
-      
-      {
-        val __value = startCharacter
-        if (__value != 0) {
-          __size += SemanticdbOutputStream.computeInt32Size(2, __value)
-        }
-      };
-      
-      {
-        val __value = endLine
-        if (__value != 0) {
-          __size += SemanticdbOutputStream.computeInt32Size(3, __value)
-        }
-      };
-      
-      {
-        val __value = endCharacter
-        if (__value != 0) {
-          __size += SemanticdbOutputStream.computeInt32Size(4, __value)
-        }
-      };
-      __size
-    }
-    override def serializedSize: _root_.scala.Int = {
-      var __size = __serializedSizeMemoized
-      if (__size == 0) {
-        __size = __computeSerializedSize() + 1
-        __serializedSizeMemoized = __size
+) extends SemanticdbGeneratedMessage
+    derives CanEqual {
+  @transient @sharable
+  private var __serializedSizeMemoized: _root_.scala.Int = 0
+  private def __computeSerializedSize(): _root_.scala.Int = {
+    var __size = 0
+
+    {
+      val __value = startLine
+      if (__value != 0) {
+        __size += SemanticdbOutputStream.computeInt32Size(1, __value)
       }
-      __size - 1
-      
+    };
+
+    {
+      val __value = startCharacter
+      if (__value != 0) {
+        __size += SemanticdbOutputStream.computeInt32Size(2, __value)
+      }
+    };
+
+    {
+      val __value = endLine
+      if (__value != 0) {
+        __size += SemanticdbOutputStream.computeInt32Size(3, __value)
+      }
+    };
+
+    {
+      val __value = endCharacter
+      if (__value != 0) {
+        __size += SemanticdbOutputStream.computeInt32Size(4, __value)
+      }
+    };
+    __size
+  }
+  override def serializedSize: _root_.scala.Int = {
+    var __size = __serializedSizeMemoized
+    if (__size == 0) {
+      __size = __computeSerializedSize() + 1
+      __serializedSizeMemoized = __size
     }
-    def writeTo(`_output__`: SemanticdbOutputStream): _root_.scala.Unit = {
-      {
-        val __v = startLine
-        if (__v != 0) {
-          _output__.writeInt32(1, __v)
-        }
-      };
-      {
-        val __v = startCharacter
-        if (__v != 0) {
-          _output__.writeInt32(2, __v)
-        }
-      };
-      {
-        val __v = endLine
-        if (__v != 0) {
-          _output__.writeInt32(3, __v)
-        }
-      };
-      {
-        val __v = endCharacter
-        if (__v != 0) {
-          _output__.writeInt32(4, __v)
-        }
-      };
-    }
-    def withStartLine(__v: _root_.scala.Int): Range = copy(startLine = __v)
-    def withStartCharacter(__v: _root_.scala.Int): Range = copy(startCharacter = __v)
-    def withEndLine(__v: _root_.scala.Int): Range = copy(endLine = __v)
-    def withEndCharacter(__v: _root_.scala.Int): Range = copy(endCharacter = __v)
-    
-    
-    
-    
-    // @@protoc_insertion_point(GeneratedMessage[dotty.tools.dotc.semanticdb.Range])
+    __size - 1
+
+  }
+  def writeTo(`_output__`: SemanticdbOutputStream): _root_.scala.Unit = {
+    {
+      val __v = startLine
+      if (__v != 0) {
+        _output__.writeInt32(1, __v)
+      }
+    };
+    {
+      val __v = startCharacter
+      if (__v != 0) {
+        _output__.writeInt32(2, __v)
+      }
+    };
+    {
+      val __v = endLine
+      if (__v != 0) {
+        _output__.writeInt32(3, __v)
+      }
+    };
+    {
+      val __v = endCharacter
+      if (__v != 0) {
+        _output__.writeInt32(4, __v)
+      }
+    };
+  }
+  def withStartLine(__v: _root_.scala.Int): Range = copy(startLine = __v)
+  def withStartCharacter(__v: _root_.scala.Int): Range =
+    copy(startCharacter = __v)
+  def withEndLine(__v: _root_.scala.Int): Range = copy(endLine = __v)
+  def withEndCharacter(__v: _root_.scala.Int): Range = copy(endCharacter = __v)
+
+  // @@protoc_insertion_point(GeneratedMessage[dotty.tools.dotc.semanticdb.Range])
 }
 
-object Range  extends SemanticdbGeneratedMessageCompanion[dotty.tools.dotc.semanticdb.Range] {
-  implicit def messageCompanion: SemanticdbGeneratedMessageCompanion[dotty.tools.dotc.semanticdb.Range] = this
-  def parseFrom(`_input__`: SemanticdbInputStream): dotty.tools.dotc.semanticdb.Range = {
+object Range
+    extends SemanticdbGeneratedMessageCompanion[
+      dotty.tools.dotc.semanticdb.Range
+    ] {
+  implicit def messageCompanion
+      : SemanticdbGeneratedMessageCompanion[dotty.tools.dotc.semanticdb.Range] =
+    this
+  def parseFrom(
+      `_input__`: SemanticdbInputStream
+  ): dotty.tools.dotc.semanticdb.Range = {
     var __startLine: _root_.scala.Int = 0
     var __startCharacter: _root_.scala.Int = 0
     var __endLine: _root_.scala.Int = 0
@@ -119,18 +125,13 @@ object Range  extends SemanticdbGeneratedMessageCompanion[dotty.tools.dotc.seman
       }
     }
     dotty.tools.dotc.semanticdb.Range(
-        startLine = __startLine,
-        startCharacter = __startCharacter,
-        endLine = __endLine,
-        endCharacter = __endCharacter
+      startLine = __startLine,
+      startCharacter = __startCharacter,
+      endLine = __endLine,
+      endCharacter = __endCharacter
     )
   }
-  
-  
-  
-  
-  
-  
+
   lazy val defaultInstance = dotty.tools.dotc.semanticdb.Range(
     startLine = 0,
     startCharacter = 0,
@@ -142,15 +143,16 @@ object Range  extends SemanticdbGeneratedMessageCompanion[dotty.tools.dotc.seman
   final val END_LINE_FIELD_NUMBER = 3
   final val END_CHARACTER_FIELD_NUMBER = 4
   def of(
-    startLine: _root_.scala.Int,
-    startCharacter: _root_.scala.Int,
-    endLine: _root_.scala.Int,
-    endCharacter: _root_.scala.Int
-  ): _root_.dotty.tools.dotc.semanticdb.Range = _root_.dotty.tools.dotc.semanticdb.Range(
-    startLine,
-    startCharacter,
-    endLine,
-    endCharacter
-  )
+      startLine: _root_.scala.Int,
+      startCharacter: _root_.scala.Int,
+      endLine: _root_.scala.Int,
+      endCharacter: _root_.scala.Int
+  ): _root_.dotty.tools.dotc.semanticdb.Range =
+    _root_.dotty.tools.dotc.semanticdb.Range(
+      startLine,
+      startCharacter,
+      endLine,
+      endCharacter
+    )
   // @@protoc_insertion_point(GeneratedMessageCompanion[dotty.tools.dotc.semanticdb.Range])
 }
